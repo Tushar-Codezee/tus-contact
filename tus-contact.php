43f60c094d1ef56a14b3d8f212fe0d112debe827
Tushar-Codezee <tushar.codezee@gmail.com>
2025-08-12 09:51:59 +0530

    initializing plugin folder structure

---
+++ tus-contact.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     Tus Contact
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     tus-contact
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Tus_Contact
+ */
+
+// Your code starts here.
+if(! defined('ABSPATH')){
+    return;
+}
